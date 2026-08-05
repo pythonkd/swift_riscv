2be80d970d563b7ccc4de7117839fc6f90240e16
zhaokunpeng <1181878670@gwm.cn>
2026-08-05 21:45:52 +0800

    [feat][debug/core] Add some mtimer code.
    Change-Id: Ibd64e1e220d87e58829ba2718aa6b13cbdf81c52
    Signed-off-by: zhaokunpeng <1181878670@gwm.cn>

--- rtl/core/mtimer.v
+++ rtl/core/mtimer.v
@@ -13,5 +13,25 @@ module mtimer(
     input rst_n,
     output mtimer_int
 );
+    localparam MTIMER0_LO = 11'h0;
+    localparam MTIMER0_HI = 11'h4;
+    localparam MTIMER0_CMP_LO = 11'h8;
+    localparam MTIMER0_CMP_HI = 11'hC;
+
+    localparam MTIMER_WIDTH = 64;
+    reg mtimer_happend;
+    reg [MTIMER_WIDTH - 1: 0]mtimer;
+    reg [MTIMER_WIDTH - 1: 0]mtimer_cmp;
+    assign mtimer_int = mtimer_happend ? 1'b1: 1'b0;
+
+    always @(posedge mtimer or negedge rst_n) begin
+        if(!rst_n)
+            mtimer_happend <= 0;
+        else if (mtimer >= mtimer_cmp) begin
+            mtimer_happend <= 1;
+        end else begin
+            mtimer_happend <= 0;
+        end
+    end
 
 endmodule
